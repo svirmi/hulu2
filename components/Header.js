27f952e0929cacfb8f05375e3ac1f18fc5359693
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-06-03 18:04:11 +0300

    text color and background color added globally

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,8 @@ function Header() {
         <header className=''>
             <h1>This is the Header</h1>
             <Image 
-                src='https://via.placeholder.com/200x100/000000/FFFFFF/?text=Hulu+2.0'
+                className="object-contain"
+                src='https://via.placeholder.com/200x100/06202A/FFFFFF/?text=Hulu+2.0'
                 width={200}
                 height={100}
             />
