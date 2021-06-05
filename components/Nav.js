1fa4b132d9f8719571cbbb9242ef6fb0bfc1afa0
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-06-05 18:18:17 +0300

    tailwind-scrollbar-hide plugin added

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import requests from "../utils/requests"
 function Nav() {
     return (
         <nav>
-            <div className="flex px-1o sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll"> 
+            <div className="flex px-1o sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"> 
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2 
                         key={key} 
