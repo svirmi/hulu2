d248bcec164b51a10279862e48e4febef6d238cf
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-06-05 18:44:16 +0300

    styling fade for the last nav element

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import requests from "../utils/requests"
 
 function Nav() {
     return (
-        <nav>
+        <nav className="relative">
             <div className="flex px-1o sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"> 
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2 
@@ -13,6 +13,7 @@ function Nav() {
                     </h2>
                 ))}
             </div>
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202a] h-10 w-1/12" />
         </nav>
     )
 }
